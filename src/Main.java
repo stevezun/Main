2c831bd5e5b3a75ae5b3bf48b4d785c4ceeef100
Steven Zuniga <szuniga@cnm.edu>
2021-03-03 16:03:02 -0700

    Added a few things

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
   public static void main(String[] args) {
         System.out.println("factorial(2) = " + factorial(2));
         System.out.println("factorial(10) = " + factorial(10));
-//        System.out.println("fibonacci(2) = " + fibonacci(2));
-//        System.out.println("fibonacci(10) = " + fibonacci(10));
-//        System.out.println("sequence1(2) = " + sequence1(2));
-//        System.out.println("sequence1(10) = " + sequence1(10));
-//        System.out.println("sequence2(2) = " + sequence2(2));
-//        System.out.println("sequence2(10) = " + sequence2(10));
+        System.out.println("fibonacci(2) = " + fibonacci(2));
+        System.out.println("fibonacci(10) = " + fibonacci(10));
+        System.out.println("sequence1(2) = " + sequence1(2));
+        System.out.println("sequence1(10) = " + sequence1(10));
+        System.out.println("sequence2(2) = " + sequence2(2));
+        System.out.println("sequence2(10) = " + sequence2(10));
 //        System.out.println("hofsQ(2) = " + hofsQ(2));
 //        System.out.println("hofsQ(10) = " + hofsQ(10));
 //        System.out.println("tetration(2, 3) = " + tetration(2, 3));
@@ -25,22 +25,26 @@ public class Main {
   }
 
   public static double fibonacci(double n) {
-    // your code goes here
-    // return 0 is a placeholder you must replace it
-    return 0;
-  }
+    if (n < 2) {
+    return n;
+    //Anything less than 2 is 1 and 0. If n is 1 the return will be 1, if n is 0 the return will be 0
+  } else {
+      return (n-1) + (n-2);
+    }
 
   public static double sequence1(double n) {
-    // your code goes here
-    // return 0 is a placeholder you must replace it
-    return 0;
-  }
+    if (n < 2){
+    return 2;
+  } else {
+      return 2 * (n-1) - 1;
+    }
 
   public static double sequence2(double n) {
-    // your code goes here
-    // return 0 is a placeholder you must replace it
-    return 0;
-  }
+    if (n < 2) {
+      return 5;
+  } else {
+      return 3 * n(n-1);
+    }
 
   public static double hofsQ(double n) {
     // your code goes here
